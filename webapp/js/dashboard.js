90b5892beb6defb2a21a6ad495028b3ccc052ae3
terminator0o0 <s.dinkar04@gmail.com>
2018-08-02 21:13:56 -0400

    Developed calendar page and continuing developing UI.

--- webapp/js/dashboard.js
+++ webapp/js/dashboard.js
@@ -1,11 +1,30 @@
 /*Dashboard*/
 
-
 function init_page() {
-	/*Initialize all materialize plugins*/
- 	M.AutoInit();
+    /*Initialize all materialize plugins*/
+    M.AutoInit();
 }
 
+function init_calendar() {
+    $('#calendar').fullCalendar({
+    	locale: 'en',
+        header: { 
+        			left: 'prev next title',
+        			right: 'month, agendaWeek, agendaDay' 
+    			},
+        views: {
+            		month: { 
+                				titleFormat: 'MMMM YYYY'
+						}	
+    			}
+    })
+}
+
+$("#calendar-start").click(function(event) {
+	/* Act on the event */
+});
+
 $(document).ready(function() {
-	//init_page();
+	init_calendar();
+    init_page();
 });
